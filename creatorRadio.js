463e2f296c38c608e0bf2fa9c7bc69a1e4a985fa
monie <mona24@gmail.com>
2025-10-21 12:59:23 -0400

    Remove commented case for 'liner' in fadeOut
    Remove commented case for 'liner' in fadeOut function.

--- creatorRadio.js
+++ creatorRadio.js
@@ -425,7 +425,6 @@
         fadeOut: function(type, song, startTime, fadeTime) {
             switch (type) {
                 case 'promo': return song.gainNode.gain.linearRampToValueAtTime(0, startTime + fadeTime);
-               // case 'liner': return  song.gainNode.gain.linearRampToValueAtTime(0, startTime + fadeTime);
                 case 'liner': return;  
                 case 'show': return;  
                 case 'music': return song.gainNode.gain.exponentialRampToValueAtTime(0.01, startTime + fadeTime);
